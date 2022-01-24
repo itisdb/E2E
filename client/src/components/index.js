ddaea8a4b57f4fa9d7697cebbf3a16c646f2a14e
itisdb <dhriteshbhagat@gmail.com>
2022-01-24 17:01:38 +0530

    Oh Snap

--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Loader } from './Loader';
-export { default as Footer } from './Footer';
-export { default as Hero } from './Hero';
-export { default as Transactions } from './Transactions';
-export { default as Navbar } from './Navbar';
-export { default as Services } from './Services';
